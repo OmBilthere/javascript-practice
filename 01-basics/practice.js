bb03b9028c49dd0656d4311b4a4e2ede949d47c9
OmBilthere <ombilthere953@gmail.com>
2025-09-08 15:27:57 +0000

    1 hour complete

---
+++ 01-basics/practice.js
@@ -0,0 +1,12 @@
+"use strict"; // phle jo javascript use hoti thi wo bhut different thi 
+// isiliye btane ke liye ki hum new use kr rhe h ye likh dete h jaroori nhi h likhna bas show krte h
+
+//alert( 3 + 3 ) we are using nodejs not browser so cant use it 
+
+let state; // this give us undefined because we didnot defined it yet
+// null => but null is diffent standalone value 
+
+//symbol => unique component ko find krne ka data type
+
+console.log(typeof null);// ye type btata h ki null object h ye kami h js ki
+// agar undefined ka type dekhenge to wo undefined hi aata h
